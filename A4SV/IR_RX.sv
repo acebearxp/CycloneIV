f6fbb5e6770bde61de899dc98b15b12512386bc2
acebearxp <acebearxp@msn.com>
2021-02-25 23:07:23 +0800

    IR step 1

--- A4SV/IR_RX.sv
+++ A4SV/IR_RX.sv
@@ -1,19 +1,37 @@
 // 红外接收器
 module IR_RX(input logic clk, input logic ir, output logic[3:0] code[2:0]);
 
+logic state;
+logic clkIR;
+logic clkSample;
+logic sample;
+logic vx;
+
+logic[7:0] counter;
+
+// 红外采样
+SAM8 sampler(.clk(clkIR), .in(ir), .clkout(clkSample), .out(vx));
+          
 initial begin
     code[2] <= 4'd0;
     code[1] <= 4'd0;
     code[0] <= 4'd0;
 end
 
-always_ff@(posedge clk) begin
-    if(ir == 0) begin
-        if(code[2] == 4'd9)
-            code[2] <= 1'd0;
-        else
-            code[2] <= code[2]+1'd1;
+always_ff@(posedge clkSample) begin
+    if(counter == 8'd127) begin
+        counter <= 8'd0;
+        
+        code[2] <= code[2]+1'd1;
     end
+    else
+        counter <= counter + 1'd1;
+end
+
+always_comb begin
+    state = (counter == 8'd0) & (~ir) || (counter != 8'd0);
+    
+    clkIR = (clk & state);
 end
 
 endmodule
---
+++ A4SV/SAM8.sv
@@ -0,0 +1,19 @@
+// 中段取样,8个时钟周期,第3个取样,第4个输出
+module SAM8(input logic clk, input logic in, output logic clkout, output logic out);
+
+logic[2:0] counter = 0;
+
+initial begin
+    clkout <= 0;
+end
+
+always_ff@(posedge clk) begin
+    counter <= counter + 3'd1;
+    
+    if(counter == 3)
+        out <= in;
+    else if(counter == 4)
+        clkout = ~clkout;
+end
+
+endmodule
